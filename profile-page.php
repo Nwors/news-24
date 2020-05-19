914eba63fddf6f3addaa840ee181938ba8377121
Nwors <8grisha8@mail.ru>
2020-05-19 17:43:19 +0500

    Fixed go-to-editor button

--- profile-page.php
+++ profile-page.php
@@ -1,8 +1,8 @@
 <link rel = "stylesheet" href = /assets/css/profile-page.css>
 <h1>Welcome <?=$user ['login'] ?> </h1>
 <h2>Your current profile image: </h2>
-<image id = "profile-image" src = <?= "/".$serverName."/uploads/".$user['image']?>></image>
+<image id = "profile-image" src = <?= $serverName."/uploads/".$user['image']?>></image>
 <?php if($user['editor']): ?>
     <h4>You've have editor's tag!</h4>
-    </a href = "/news-editor"><button>Go to news editor!</button>
+    </a href = <?=$serverName."/news-editor"?>><button>Go to news editor!</button>
 <?php endif; ?>
